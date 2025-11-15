56799dbb860505f6fdeaea4bcc066605f4a40d6e
Acuiza <Acuiza@hypernovalabs.com>
2025-11-14 23:42:51 -0400

    first Commit for project backend

---
+++ src/test/java/com/summary_ai/SummaryAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summary_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummaryAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
