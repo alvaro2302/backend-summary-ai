63c4d1624d05d30dbdb923f7278763f4357b593d
Acuiza <Acuiza@hypernovalabs.com>
2025-11-17 10:17:52 -0400

    configurate properties from dev and prod

---
+++ src/main/java/com/summary_ai/web/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.summary_ai.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return  "Hello world";
+    }
+}
